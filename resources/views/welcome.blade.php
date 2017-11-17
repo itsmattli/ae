0fd8e8ff99466a85814f04d28bdbfca5bf7314b3
Matthew Li <li.matthew.m@gmail.com>
2017-11-17 13:42:52 -0800

    cleaned up welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,20 @@
 @extends('layouts.app')
 @section('pageTitle', 'Matthew Li - Web Developer')
 @section('content')
-    <div>
-        <ul>
-            <li><a href="/castles">Castles</a></li>
-            <li><a href="/transformers">Transformers</a></li>
-        </ul>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Aequilibrium Assignment</div>
+                    <div class="panel-body">
+                        <ul>
+                            <li><a href="/castles">Castles</a></li>
+                            <li><a href="/transformers">Transformers</a></li>
+                            <li><a href="http://matthewli.xyz">Visit My Website!</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
